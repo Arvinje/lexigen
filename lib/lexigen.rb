c18ee5e3cd6dc9444ffa481aea077318e612c93a
Arvin Jenabi <Arvinje@gmail.com>
2015-10-11 13:15:05 +0330

    Add a simple way to construct state machines.

--- lib/lexigen.rb
+++ lib/lexigen.rb
@@ -1,5 +1,19 @@
 require "lexigen/version"
+require "matrix"
+require "lexigen/state_machine"
 
 module Lexigen
-  # Your code goes here...
+
+  # Easily define a set of state machines and their transitions
+  # @example
+  # Lexigen.define do |sm|
+  #   sm.from(0).to(2).if(/d+/)
+  #   sm.from(2).to(1).unless(/w+/)
+  # end
+  def self.define
+    definition = StateMachine.new
+    yield definition
+    definition.to_matrix
+  end
+  
 end
--- spec/lexigen_spec.rb
+++ spec/lexigen_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
+require "matrix"
 
 describe Lexigen do
   it 'has a version number' do
     expect(Lexigen::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe ".define" do
+    before do
+      @state_machine = Lexigen.define do |sm|
+        sm.from(0).to(2).if(/d+/)
+        sm.from(2).to(1).unless(/w+/)
+      end
+    end
+
+    it "returns the constructed state machine" do
+      expect(@state_machine.matrix).to eql Matrix[ [nil,nil,{if: /d+/, unless: nil}],
+                                                   [nil,nil,nil], [nil,{if: nil, unless: /w+/},nil] ]
+    end
   end
+
 end
